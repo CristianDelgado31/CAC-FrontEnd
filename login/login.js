31fa38ce50ab1cd45cb623e03c18c1ce56dda21d
CristianDelgado31 <cristian-31-10@hotmail.com>
2024-05-31 23:25:09 -0300

    cambios en login

--- login/login.js
+++ login/login.js
@@ -1,12 +1,9 @@
-function validarCorreo() {
-    const emailInput = document.getElementById('email');
-    const emailValue = emailInput.value;
-    const regex = /^[a-zA-Z0-9._%+-]+@(gmail\.com|hotmail\.com)$/;
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    const emailInput = document.getElementById('email').value;
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-    if (!regex.test(emailValue)) { // Si el correo no cumple con el patrón establecido (gmail.com o hotmail.com) se muestra un mensaje de error
+    if (!regex.test(emailInput)) {
+        event.preventDefault();
         alert('Por favor, ingrese un correo electrónico válido.');
-        return false;
     }
-
-    return true;
-}
+});
